e779d01f16b097df00760a20a91cb4453536f55b
Alex Sakic <42554940+Bor030@users.noreply.github.com>
2024-12-10 19:16:21 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 const PORT = 4000
-const DB = "mongodb+srv://app1user:aleksandar030@cluster0.uoedn.mongodb.net/social-app?retryWrites=true&w=majority&appName=Cluster0"
+const DB = ""
 
 module.exports = {PORT, DB}
